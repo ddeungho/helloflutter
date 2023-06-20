1e08ae13cd2a11d1e6f5990c5c9a7e1f4c74d7b0
sh <melgidak12@gmail.com>
2023-06-20 12:50:52 +0900

    플러터 앱 처음 만들어 보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,178 @@
+
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Exercise Recommendation',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/exercise': (context) => ExerciseScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
+class HomeScreen extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('홈'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 600,
+              height: 600,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Image.asset(
+                  'assets/images/Home.png',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/exercise');
+              },
+              child: Text('운동 화면으로 이동'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int leftDice = 0;
-  List<String> menuList = [
-    "karts",
-    "lamen",
-    "steak",
-    "chicken",
-    "pizza",
-    "hamburger"
+
+class ExerciseScreen extends StatelessWidget {
+  final List<String> exercises = [
+    'push-up',
+    'squats',
+    'lunge',
+    'sit-up',
+    'plank',
   ];
-  String menuName = "agumon";
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      leftDice = Random().nextInt(menuList.length);
-      menuName = menuList[leftDice];
-    });
+
+  final Map<String, String> exerciseDescriptions = {
+    'push-up': '팔을 힘껏 폈다 폈다 하는 운동입니다.',
+    'squats': '다리를 굽혀 앉았다 일어납니다.',
+    'lunge': '한쪽 다리로 앞으로 한 발자국 내디뎠다 원래 자리로 돌아옵니다.',
+    'sit-up': '등을 바닥에 대고 상체를 일으키는 운동입니다.',
+    'plank': '양 손바닥과 발끝만 바닥에 대고 몸을 일직선으로 유지합니다.',
+  };
+
+  void recommendExercise(BuildContext context, String exercise) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Exercise Recommendation'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(exercise),
+              SizedBox(height: 10),
+              Text(exerciseDescriptions[exercise]!),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Exercise Recommendation'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '오늘의 메뉴 추천',
-            ),
-            Text(
-              menuName,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Image.asset('assets/$menuName.png', width: 400, height: 300, fit: BoxFit.fill),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/push-up.png',
+                width: 200,
+                height: 200,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  recommendExercise(context, 'push-up');
+                },
+                child: Text('push-up'),
+              ),
+              Image.asset(
+                'assets/images/squat.png',
+                width: 200,
+                height: 200,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  recommendExercise(context, 'squats');
+                },
+                child: Text('squats'),
+              ),
+              Image.asset(
+                'assets/images/lunge.png',
+                width: 200,
+                height: 200,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  recommendExercise(context, 'lunge');
+                },
+                child: Text('lunge'),
+              ),
+              Image.asset(
+                'assets/images/sit-up.png',
+                width: 200,
+                height: 200,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  recommendExercise(context, 'sit-up');
+                },
+                child: Text('sit-up'),
+              ),
+              Image.asset(
+                'assets/images/plank.png',
+                width: 200,
+                height: 200,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  recommendExercise(context, 'plank');
+                },
+                child: Text('plank'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
